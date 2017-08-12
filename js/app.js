0c4135bed33d7562c4c4654483bc88952bf3fff7
Karan Motani <karan.motani94@gmail.com>
2017-08-12 10:59:22 -0500

    changes using mustachejs

--- js/app.js
+++ js/app.js
@@ -1,130 +1,199 @@
-function createTable() {
-
-	var matA_row = document.getElementById("matA-row").value;
-	var matA_col = document.getElementById("matA-col").value;
-	var matB_row = document.getElementById("matB-row").value;
-	var matB_col = document.getElementById("matB-col").value;
 
 
+MyApp = (function(){
 
-	for(var i = 0; i < matA_row; i++) {
+	let app = {};
 
-		for(var j = 0; j < matA_col; j++) {
-			
-			var textfield = document.createElement('input');
-			textfield.type = "number";
-			textfield.id = 'matA' + i + j;
-			var output = document.getElementById("matA");
-			output.appendChild(textfield);
+	// let utils = {};
 
+	app.state = {
+		matrix1 : {
+			cols : 0,
+			rows: 0
+		},
+		matrix2 : {
+			cols : 0,
+			rows: 0
+		}
+	};
+
+	// Callback for user inputs
+
+	function _onKeyUp( evt ){
+
+		console.log(this.id);
+
+		switch(this.id){
+
+			case "matArow":
+			app.state.matrix1.rows = this.value;
+			break;
+
+			case "matBrow":
+			app.state.matrix2.rows = this.value;
+			break;
+
+			case "matAcol":
+			app.state.matrix1.cols = this.value;
+			break;
+
+			case "matBcol":
+			app.state.matrix2.cols = this.value;
+			break;
 		}
 
-		var br = document.createElement('br');
-		var setBreak = document.getElementById("matA");
-		setBreak.appendChild(br);
+		console.log(app.state)
+
+	}
+
+	// Callback for answer button
+
+	function _createTable( evt ) {
+
+		let matrix1 = app.state.matrix1;
+		let matrix2 = app.state.matrix2;
+
+		var matA = mustacheMatrix('matA',matrix1.rows,matrix1.cols)
+		var matB = mustacheMatrix('matB',matrix2.rows,matrix2.cols)
+
+		$('#matA').append(matA);
+		$('#matB').append(matB);
+
 	}
 
 
-	for(var i = 0; i < matB_row; i++) {
+	function _createMatrix() {
 
-		for(var j = 0; j < matB_col; j++) {
+		let matrix1 = app.state.matrix1;
+		let matrix2 = app.state.matrix2;
 
-			var textfield = document.createElement('input');
-			textfield.type = "number";
-			textfield.id = 'matB' + i + j;
-			var output = document.getElementById("matB");
-			output.appendChild(textfield);
-
-		}
-
-		var br = document.createElement('br');
-		var setBreak = document.getElementById("matB");
-		setBreak.appendChild(br);
-	}
-}
+		var matrixA = new Array(matrix1.rows);
+		var matrixB = new Array(matrix2.rows);
 
 
-function createMatrix() {
+		for(var i = 0; i < matrix1.rows; i++) {
 
-	
-	var matA_row = document.getElementById("matA-row").value;
-	var matA_col = document.getElementById("matA-col").value;
-	var matB_row = document.getElementById("matB-row").value;
-	var matB_col = document.getElementById("matB-col").value;
+			matrixA[i] = new Array(matrix1.cols);
 
-	var matrixA = new Array(matA_row);
-	var matrixB = new Array(matB_row);
-
-
-	for(var i = 0; i < matA_row; i++) {
-
-		matrixA[i] = new Array(matA_col);
-
-		for(var j = 0; j < matA_col; j++) {
-			
-			matrixA[i][j] = Number(document.getElementById('matA' + i + j).value);
-		}
-	}
-
-
-	for(var i = 0; i < matB_row; i++) {
-
-		matrixB[i] = new Array(matB_col);
-
-		for(var j = 0; j < matB_col; j++) {
-			
-			matrixB[i][j] = Number(document.getElementById('matB' + i + j).value);
-		}
-	}
-
-	matrixMultiplication(matrixA, matrixB);
-
-
-}
-
-
-function matrixMultiplication(matrixA, matrixB) {
-
-	Arow = matrixA.length;
-	Acol = matrixA[0].length;
-	Brow = matrixB.length;
-	Bcol = matrixB[0].length;
-
-	var answer = new Array(Arow);
-	console.log('Matrix A: ' + Arow + ' * ' + Acol);
-	console.log('Matrix B: ' + Brow + ' * ' + Bcol);
-
-	if(Acol === Brow) {
-
-		for(var row = 0; row < answer.length; row++) {
-
-			answer[row] = new Array(matrixB[row].length);
-
-			for(var col = 0; col < Arow; col++) {
-
-				answer[row][col] = 0;
-
-				for(var x = 0; x < Brow; x++) {
-
-					answer[row][col] += matrixA[row][x] * matrixB[x][col];
-				}
+			for(var j = 0; j < matrix1.cols; j++) {
+				
+				matrixA[i][j] = Number(document.getElementById('matA' + i + j).value);
 			}
 		}
 
-		console.log('Multiplication successfull');
+
+		for(var i = 0; i < matrix2.rows; i++) {
+
+			matrixB[i] = new Array(matrix2.cols);
+
+			for(var j = 0; j < matrix2.cols; j++) {
+				
+				matrixB[i][j] = Number(document.getElementById('matB' + i + j).value);
+			}
+		}
+
+		let answer = matrixMultiplication(matrixA, matrixB);
+		
 		document.getElementById("result").value = answer;
 
-		for(var i = 0; i < answer.length; i++) {
-			console.log(answer[i]);
+	}
+
+
+	/*
+	 * @params: 
+	 *	prefix - 'prefix name of id'
+	 *  row - 'row number'
+	 *  cols - 'col number'
+	 */
+
+
+	function mustacheMatrix(prefix,rows,cols){
+
+		var idArr = [], output="";
+		var tpl = $('#cellTpl').html();
+
+		for(var i = 0; i < rows; i++) {
+			for(var j = 0; j < cols; j++) {
+				
+				idArr.push({
+					id: prefix + i + j,
+					col: j,
+					row: i,
+					isFull : function(r){
+						return (parseInt(this.col)%parseInt(cols)-1 === 0)?true:false
+					}
+				});
+			}
 		}
+		return Mustache.render(tpl, { data: idArr });
+
 	}
 
-	else {
-		document.getElementById("result").value = 'Multiplication not successfull';
-		console.log('Multiplication not successfull');
+	
+	 app.matrixMultiplication = function(matrixA, matrixB) {
+
+		Arow = matrixA.length;
+		Acol = matrixA[0].length;
+		Brow = matrixB.length;
+		Bcol = matrixB[0].length;
+
+		var answer = new Array(Arow);
+		
+		console.log('Matrix A: ' + Arow + ' * ' + Acol);
+		console.log('Matrix B: ' + Brow + ' * ' + Bcol);
+
+		if(Acol === Brow) {
+
+			for(var row = 0; row < answer.length; row++) {
+
+				answer[row] = new Array(matrixB[row].length);
+
+				for(var col = 0; col < Arow; col++) {
+
+					answer[row][col] = 0;
+
+					for(var x = 0; x < Brow; x++) {
+
+						answer[row][col] += matrixA[row][x] * matrixB[x][col];
+					}
+				}
+			}
+
+			console.log('Multiplication successfull');
+			for(var i = 0; i < answer.length; i++) {
+				console.log(answer[i]);
+			}
+		}
+		else {
+			answer = 'Multiplication not successfull';
+			console.log('Multiplication not successfull');
+		}
+
+		return answer;
 	}
 
-}
+	
+	app.init = function(){
+
+		$('#generateButton').click(_createTable);
+		$('#answerButton').click(_createMatrix);
+		$("input.userInputs").keyup(_onKeyUp)
+
+	}
+
+	app.render = function(){
+		// Render this application to screen
+		let template = $('#mainTpl').html()
+		$('body').html(template);
+	};
+
+	// $.apply({},app,utils);
+
+	return app;
+
+})();
+
+
 
 //matrixA = [[1, 2, 3], [4, 5, 6]];
 //matrixB = [[7, 8], [9, 10], [11, 12]];
